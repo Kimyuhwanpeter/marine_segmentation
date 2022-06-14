8fbdab5f4d14285d7a0801b00790345561777984
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-06-14 19:46:29 +0900

    Create Cal_measurement.py

---
+++ Cal_measurement.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import tensorflow as tf
+import numpy as np
+
+class Measurement:
+    def __init__(self, predict, label, shape, total_classes):
+        self.predict = predict
+        self.label = label
+        self.total_classes = total_classes
+        self.shape = shape
+
+    def MIOU(self):
+        # 0-crop, 1-weed, 2-background
+        self.predict = np.reshape(self.predict, self.shape)
+        #predict_ = np.array([self.predict[1], self.predict[0]])
+        self.label = np.reshape(self.label, self.shape)
+        #label_ = np.array([self.label[1], self.label[0]])
+
+        #predict_count = np.bincount(self.predict, minlength=self.total_classes)
+        #label_count = np.bincount(self.label, minlength=self.total_classes)
+
+        #temp = self.total_classes * np.array(self.label, dtype="int") + np.array(self.predict, dtype="int")  # Get category metrics
+
+        #temp_count = np.bincount(temp, minlength=self.total_classes * self.total_classes)
+        #cm = np.reshape(temp_count, [self.total_classes, self.total_classes])
+        #cm = np.diag(cm)
+
+        cm = tf.math.confusion_matrix(self.label, 
+                                      self.predict,
+                                      num_classes=self.total_classes).numpy()
+
+        return cm
