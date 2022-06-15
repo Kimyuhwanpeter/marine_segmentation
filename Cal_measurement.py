5ade9ce7cb7baf157fccdc96119848a7653063b4
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-06-15 21:20:30 +0900

    Update Cal_measurement.py

--- Cal_measurement.py
+++ Cal_measurement.py
@@ -16,17 +16,23 @@ class Measurement:
         self.label = np.reshape(self.label, self.shape)
         #label_ = np.array([self.label[1], self.label[0]])
 
-        #predict_count = np.bincount(self.predict, minlength=self.total_classes)
-        #label_count = np.bincount(self.label, minlength=self.total_classes)
+        predict_count = np.bincount(self.predict, minlength=self.total_classes)
+        label_count = np.bincount(self.label, minlength=self.total_classes)
 
-        #temp = self.total_classes * np.array(self.label, dtype="int") + np.array(self.predict, dtype="int")  # Get category metrics
+        temp = self.total_classes * np.array(self.label, dtype="int") + np.array(self.predict, dtype="int")  # Get category metrics
 
-        #temp_count = np.bincount(temp, minlength=self.total_classes * self.total_classes)
-        #cm = np.reshape(temp_count, [self.total_classes, self.total_classes])
-        #cm = np.diag(cm)
+        temp_count = np.bincount(temp, minlength=self.total_classes * self.total_classes)
+        cm = np.reshape(temp_count, [self.total_classes, self.total_classes])
+        cm = np.diag(cm)
+
+        U = label_count + predict_count - cm
+
+        out = np.zeros((self.total_classes))
+        miou = np.divide(cm, U, out=out, where=U != 0)
+        miou = np.nanmean(miou)
 
         cm = tf.math.confusion_matrix(self.label, 
                                       self.predict,
                                       num_classes=self.total_classes).numpy()
 
-        return cm
+        return miou, cm
